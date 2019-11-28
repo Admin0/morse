381f21d139a45b782feb2c8eaf4ee54c6fb32ff5
JinH <jinh719@gmail.com>
2019-11-29 01:40:05 +0900

    3.1.7

--- js/sub__i18n.js
+++ js/sub__i18n.js
@@ -4,44 +4,49 @@ $.i18n().load({
   'ja': 'i18n/ja.json'
 });
 
-function i18n_message() {
-  if ($('#message').text() == "MESSAGE") {
-    const list_message = [];
-    for (var i = 0; i < 99; i++) {
-      var m = $.i18n('message_' + i);
-      if (m != 'message_' + i) {
-        // console.log('ddd: ' + i);
-        list_message.push(m);
-      } else {
-        break;
+const i18n = {
+  set: function() {
+    // $.i18n().locale = "ja";
+
+    $('[data-i18n]').i18n();
+    $('#input_textarea').attr('placeholder', $.i18n('input_textarea_placeholder_0'));
+    $('#html').attr('placeholder', $.i18n('input_textarea_placeholder_0'));
+
+    $("meta[name='description']").attr("content", $.i18n('descript') + ' ' + $.i18n('translator') + ' + ' + $.i18n('analyzer'));
+
+    i18n.message.set();
+
+    // if ($('.card_header.auto').text() == 'lang_auto') {
+    if ($('#i18n_checker').text() == '#morse' || $('#i18n_checker').length == 0) {
+      setTimeout(function() {
+        console.log('ERROR: i18n was not activated because DOM is not ready.');
+        i18n.set();
+      }, 100);
+    } else {
+      console.log('i18n was activated.');
+    }
+  },
+  message: {
+    list: [],
+    set: function() {
+      if ($('#message').text() == "MESSAGE") {
+        for (var i = 0; i < 99; i++) {
+          var m = $.i18n('message_' + i);
+          if (m != 'message_' + i) {
+            // console.log('ddd: ' + i);
+            this.list.push(m);
+          } else {
+            break;
+          }
+        }
+        $('#message')
+          .html(this.list[Math.floor(Math.random() * (this.list.length))])
+          .removeClass('hide');
       }
     }
-    $('#message').html(list_message[Math.floor(Math.random() * (list_message.length))]);
-  }
-}
-
-function i18n_set() {
-  // $.i18n().locale = "ja";
-
-  $('[data-i18n]').i18n();
-  $('#input_textarea').attr('placeholder', $.i18n('input_textarea_placeholder_0'));
-  $('#html').attr('placeholder', $.i18n('input_textarea_placeholder_0'));
-
-  $("meta[name='description']").attr("content", $.i18n('descript') + ' ' + $.i18n('translator') + ' + ' + $.i18n('analyzer'));
-
-  i18n_message();
-
-  // if ($('.card_header.auto').text() == 'lang_auto') {
-  if ($('#i18n_checker').text() == '#morse' || $('#i18n_checker').length == 0) {
-    setTimeout(function() {
-      console.log('ERROR: i18n was not activated because DOM is not ready.');
-      i18n_set();
-    }, 100);
-  } else {
-    console.log('i18n was activated.');
   }
 }
 
 $(document).ready(function() {
-  i18n_set();
+  i18n.set();
 });
