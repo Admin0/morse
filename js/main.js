a9d1c73f0c0a7eb5ac6b0b85c138008512e38fa3
JinH <jinh719@gmail.com>
2020-02-01 04:20:26 +0900

    3.6.0 PWA.

--- js/main.js
+++ js/main.js
@@ -346,7 +346,7 @@ function initialize_langDropDown() {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('main__service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
