28d659ab84fde5cd06eb9ce93df38972333f2c5f
JinH <jinh719@gmail.com>
2020-02-01 04:15:31 +0900

    3.6.0 PWA.

--- js/main.js
+++ js/main.js
@@ -344,6 +344,18 @@ function initialize_langDropDown() {
   });
 }
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('main__service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 $(function() {
   initialize();
   initialize_langDropDown();
